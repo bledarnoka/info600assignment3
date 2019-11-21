e0b9d3c67751ae79e35525b869597b418f3488d0
apple <apple@n3-31-221.dhcp.drexel.edu>
2019-11-21 18:08:42 -0500

    Bledar Noka

--- js/main.js
+++ js/main.js
@@ -1,26 +1,120 @@
 document.addEventListener('DOMContentLoaded', assignClickHandler)
 
-function assignClickHandler () {
+
+document.addEventListener('DOMContentLoaded', assignClickHandlerLoadData)
+
+
+
+function assignClickHandler() {
+
   document.getElementById('addRec').addEventListener('click', function () {
+
     const startYear = document.getElementById('startYear').value
+
     if (startYear <= 2000) {
       window.alert('Incorrect year: ' + startYear)
       return
     }
+
     const fullName = document.getElementById('fullName').value
+
     const major = document.getElementById('major').value
 
     const date = new Date()
+
     const time = date.getHours() + ':' + date.getMinutes()
 
     const newEntry = time + ' - ' + fullName + ', ' + major + ', ' + startYear
 
     const enteredRecords = document.getElementById('enteredRecords')
-    let newChild = document.createElement('li')
-    newChild.appendChild(document.createTextNode(newEntry))
 
-    enteredRecords.appendChild(newChild)
+    let ne = document.createElement('li')
 
-    document.getElementById('inputs').reset()
+    ne.append(document.createTextNode(newEntry))
+    $.ajax({
+				method: 'POST',
+				url: '/users',
+				type: 'POST',
+
+				cache: false,
+				data: {
+					fullName:$('#fullName').val(),
+					major:$('#major').val(),
+					startYear: $('#startYear').val(),
+				}
+			});
+			console.log("it is working");
+      document.getElementById('inputs').reset();
+			});
+
+			}
+
+function assignClickHandlerLoadData() {
+
+  document.getElementById('loadData').addEventListener('click', function () {
+
+    var theRequest = new XMLHttpRequest();
+
+    theRequest.open('GET', '/users') // hitting the URL to retrieve the student objects
+
+    theRequest.onload = function () {
+
+      var theData = JSON.parse(theRequest.responseText) // parsing the String response into JSON format
+
+
+      for (i = 0; i < theData.records.length; i++) { // looping through the JSON objects (i.e. students)
+
+        var fullName = theData.records[i].fullName
+
+        var major = theData.records[i].major
+
+        var startYear = theData.records[i].startYear
+
+      	$("#enteredRecords").append(theData.records[i].fullName + " , " + theData.records[i].major + " ,  " + theData.records[i].startYear + "<button value='" +theData.records[i].id+ "' id ='deleteData'>Delete</button>" +"<br>" );
+
+      }
+
+
+    }
+
+    theRequest.send();
+
   })
+
+  $(document).on("click","#deleteData", function deleteData()
+                {
+                  const id = $(this).val();
+                  console.log(id);
+                  $.ajax
+                  ({
+                    url: '/user/'+ id,
+                    method: 'DELETE',
+
+                  });
+
+                  alert("Record deleted");
+
+                  var theRequest = new XMLHttpRequest();
+
+                  theRequest.open('GET', '/users') // hitting the URL to retrieve the student objects
+
+                   theRequest.onload = function() {
+
+                    var theData = JSON.parse(theRequest.responseText) // parsing the String response into JSON format
+
+
+                    for (i = 0; i < theData.records.length; i++) { // looping through the JSON objects (i.e. students)
+
+                      var fullName = theData.records[i].fullName
+
+                      var major = theData.records[i].major
+
+                      var startYear = theData.records[i].startYear
+
+                    	$("#enteredRecords").append(theData.records[i].fullName + " , " + theData.records[i].major + " ,  " + theData.records[i].startYear + "<button value='" +theData.records[i].id+ "' id ='deleteData'>Delete</button>" +"<br>" );
+
+                    }
+
+                  }
+                });
 }
--- server.py
+++ server.py
@@ -1,94 +1,197 @@
 from flask import Flask, request, send_from_directory, make_response
+ 
 import json
+ 
 import uuid
+ 
 from shutil import copyfile
-
+ 
+ 
+ 
 app = Flask(__name__, static_url_path='')
-
+ 
+ 
+ 
 # Main route to display form.html for the root index
-
+ 
+ 
+ 
+ 
+ 
 @app.route('/')
+ 
 def index():
-  return send_from_directory('.', 'form.html')
-
-
+ 
+    return send_from_directory('.', 'form.html')
+ 
+ 
+ 
+ 
+ 
 # Routes for static resources
-
+ 
+ 
+ 
 @app.route('/js/<path:path>')
+ 
 def get_js(path):
+ 
     return send_from_directory('js', path)
-
+ 
+ 
+ 
+ 
+ 
 @app.route('/css/<path:path>')
+ 
 def get_css(path):
+ 
     return send_from_directory('css', path)
-
+ 
+ 
+ 
 # Route to retrieve data file
+ 
 @app.route('/data/<path:path>')
+ 
 def get_data(path):
+ 
     return send_from_directory('data', path)
-
-
+ 
+ 
+ 
+ 
+ 
 # HTTP actions for assignment 3
-
-@app.route('/users', methods = ['GET'])
+ 
+ 
+ 
+@app.route('/users', methods=['GET'])
+ 
 def getUsers():
+ 
     with open('data/entries.json', 'r') as f:
+ 
         d = json.load(f)
+ 
         return(d)
-
-@app.route('/user/', methods = ['GET'])
+ 
+ 
+ 
+ 
+ 
+@app.route('/users', methods=['POST','GET'])
+ 
 def addUser():
+ 
     newId = uuid.uuid4().hex[:6]
-
+ 
+ 
+ 
     newUser = {
+ 
         "id": newId,
+ 
         "fullName": request.form.get("fullName"),
+ 
         "major": request.form.get("major"),
+ 
         "startYear": int(request.form.get("startYear"))
+ 
     }
-
+ 
+ 
+ 
     data = ''
+ 
     fileName = 'data/entries.json'
+ 
     with open(fileName, 'r') as f:
+ 
         # Read the JSON into a variable
+ 
         data = json.load(f)
-
+ 
+ 
+ 
         # Add a new record to the JSON
+ 
         data["records"].append(newUser)
-
-    writeToFile(data, fileName)
-
-@app.route('/user/<user_id>', methods = ['GET'])
-def deleteUser(user_id):
-    data = ''
-    fileName = 'data/entries.json'
-    with open(fileName, 'r') as f:
-        data = json.load(f)
-
-    # Iterate through records, delete one that matches user id
-    for i, record in enumerate(data["records"]):
-        if record["id"] == user_id:
-            del data["records"][i]
-
+ 
+ 
+ 
     writeToFile(fileName, data)
+ 
+ 
+ 
+ 
+ 
+@app.route('/user/<user_id>', methods=['DELETE'])
+ 
+def deleteUser(user_id):
+ 
+    data = ''
+ 
+    fileName = 'data/entries.json'
+ 
+    with open(fileName, 'r') as f:
+ 
+        data = json.load(f)
+ 
+ 
+ 
+    # Iterate through records, delete one that matches user id
+ 
+    for i, record in enumerate(data["records"]):
+ 
+        if record["id"] == user_id:
+ 
+            del data["records"][i]
+ 
+ 
+ 
+    writeToFile(fileName, data)
+ 
     return make_response('', 200)
-
-
+ 
+ 
+ 
+ 
+ 
 def writeToFile(filePath, jsonString):
+ 
     with open(filePath, 'w') as f:
+ 
         # Write the modified list to file
+ 
         json.dump(jsonString, f, sort_keys=True, indent=4)
-
+ 
+ 
+ 
+ 
+ 
 if __name__ == '__main__':
-  # If you mess up your data, re-run the container and it will be restored
-  copyfile('data/entries_orig.json', 'data/entries.json')
-
-  app.run(host='0.0.0.0', port=8081, debug=True)
-      
-
+ 
+    # If you mess up your data, re-run the container and it will be restored
+ 
+    copyfile('data/entries_orig.json', 'data/entries.json')
+ 
+ 
+ 
+    app.run(host='0.0.0.0', port=8081, debug=True)
+ 
+ 
+ 
+ 
+ 
 '''
- Build the Docker image:
+ 
+Build the Docker image:
+ 
    docker image build -t assignment3 .
- Run the Docker image:
+ 
+Run the Docker image:
+ 
    docker run --rm -it --mount src="$(pwd)",target=/app,type=bind -p 8081:8081 assignment3
+ 
 '''
